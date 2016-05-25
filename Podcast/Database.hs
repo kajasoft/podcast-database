0db181623a24713c369d7b4554a096dcb5304f78
Daniel Choi <dhchoi@gmail.com>
2016-05-25 16:52:14 -0400

    more dependencies, postgresql-simple

--- Podcast/Database.hs
+++ Podcast/Database.hs
@@ -1,4 +1,19 @@
-{-# LANGUAGE OverloadedStrings, RecordWildCards, ScopedTypeVariables #-} 
+{-# LANGUAGE OverloadedStrings, RecordWildCards, ScopedTypeVariables, QuasiQuotes #-} 
 module Podcast.Database where
 import Podcast.Types
+import Database.PostgreSQL.Simple
+import Database.PostgreSQL.Simple.FromRow
+import Database.PostgreSQL.Simple.SqlQQ
+import qualified Data.ByteString.Lazy as BL
+import Control.Applicative
+import Data.Aeson
+import Data.Text (Text)
+import qualified Data.Text as T
+import Data.Time.Calendar (Day)
+import qualified Data.Map as M
+import Data.Maybe (catMaybes)
+import Data.Text.Read (decimal, double)
+import Data.Monoid
+import Podcast.Types
+
 
