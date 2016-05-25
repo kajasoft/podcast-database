4f1cdc9566cd2e3f4ba59ae4c21092c88c8a1c6c
Daniel Choi <dhchoi@gmail.com>
2016-05-25 17:03:24 -0400

    database code

--- Podcast/Database.hs
+++ Podcast/Database.hs
@@ -1,4 +1,4 @@
-{-# LANGUAGE OverloadedStrings, RecordWildCards, ScopedTypeVariables, QuasiQuotes #-} 
+{-# LANGUAGE OverloadedStrings, RecordWildCards, ScopedTypeVariables, QuasiQuotes, FlexibleInstances #-} 
 module Podcast.Database where
 import Podcast.Types
 import Database.PostgreSQL.Simple
@@ -15,5 +15,57 @@ import Data.Maybe (catMaybes)
 import Data.Text.Read (decimal, double)
 import Data.Monoid
 import Podcast.Types
+
+data EntityFeed = EntityFeed {
+      feedId :: Int
+    , eFeed :: Feed
+    }
+
+decimalFromText :: Text 
+                -> Int    -- ^ default 
+                -> Int
+decimalFromText x def = either (const def) fst $ decimal x
+
+baseQuery = [sql|
+        select title_id, title_uri, title, year, content_type, 
+        maturity_rating_value, synopsis,
+        actors_json, directors_json, creators_json, genres_json,
+        ((average_rating * 10)::integer) as average_rating,
+        awards_json, languages,
+        webpage, tinyurl, runtime,
+        series_id, series_title, season_id, season_title, season_number, 
+        program_sequence, program_count, children_ids, similars_ids, 
+        available_from,
+        expires_on,
+        is_hd, is_super_hd, is_ultra_hd, box_art_json,
+        rt_id,
+        rt_critics_rating, rt_critics_score,
+        rt_audience_rating, rt_audience_score, 
+        nyt_review_id, nyt_critics_pick,
+        queue_count, queue_count_today
+        from titles |]
+
+fetchFeeds :: Connection -> [Int] -> IO [EntityFeed]
+fetchFeeds c ids = do
+    let q = (baseQuery <> " where feed_id in ?")
+    xs <- query c q $ Only (In ids)
+    -- order by original ids
+    let xs' = M.fromList $ map (\x@EntityFeed{..} -> (feedId, x)) xs
+    let xs'' = catMaybes $ map (\i -> M.lookup i xs') ids
+    return xs''
+
+instance FromRow EntityFeed where
+  fromRow = EntityFeed
+    <$> field
+    <*> (Feed 
+        <$> field
+        <*> field
+        <*> field
+        <*> field
+        <*> field
+        <*> field -- keywords
+        <*> field -- categories
+        <*> field
+        )
 
 
